1d002cf3162a1bd433a1dfe76edac181474ff8cf
TZQ <zhengqiang.tan@zhangmen.com>
2020-09-21 17:21:00 +0800

    自定义过滤器监听器拦截器

--- src/main/java/com/project/tan/config/WebConfig.java
+++ src/main/java/com/project/tan/config/WebConfig.java
@@ -1,16 +1,8 @@
 package com.project.tan.config;
 
-import com.project.tan.filter.MyFilter;
-import com.project.tan.filter.MyHttpRequestListener;
-import com.project.tan.filter.MyHttpSessionListener;
-import com.project.tan.filter.MyInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -29,40 +21,41 @@ public class WebConfig implements WebMvcConfigurer {
         return new RestTemplate();
     }
 
-    @Autowired
-    MyInterceptor myInterceptor;
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(myInterceptor);
-    }
-
-
-    /**
-     * 注册过滤器
-     *
-     * @return
-     */
-    @Bean
-    public FilterRegistrationBean filterRegistrationBean() {
-        FilterRegistrationBean filterRegistration = new FilterRegistrationBean();
-        filterRegistration.setFilter(new MyFilter());
-        filterRegistration.addUrlPatterns("/*");
-        return filterRegistration;
-    }
-
-
-    /**
-     * 注册监听器
-     *
-     * @return
-     */
-    @Bean
-    public ServletListenerRegistrationBean registrationBean() {
-        ServletListenerRegistrationBean registrationBean = new ServletListenerRegistrationBean();
-        registrationBean.setListener(new MyHttpRequestListener());
-        registrationBean.setListener(new MyHttpSessionListener());
-        return registrationBean;
-    }
+    // 暂时取消测试，不然每次测试都会输出很多log
+//    @Autowired
+//    MyInterceptor myInterceptor;
+//
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(myInterceptor);
+//    }
+//
+//
+//    /**
+//     * 注册过滤器
+//     *
+//     * @return
+//     */
+//    @Bean
+//    public FilterRegistrationBean filterRegistrationBean() {
+//        FilterRegistrationBean filterRegistration = new FilterRegistrationBean();
+//        filterRegistration.setFilter(new MyFilter());
+//        filterRegistration.addUrlPatterns("/*");
+//        return filterRegistration;
+//    }
+//
+//
+//    /**
+//     * 注册监听器
+//     *
+//     * @return
+//     */
+//    @Bean
+//    public ServletListenerRegistrationBean registrationBean() {
+//        ServletListenerRegistrationBean registrationBean = new ServletListenerRegistrationBean();
+//        registrationBean.setListener(new MyHttpRequestListener());
+//        registrationBean.setListener(new MyHttpSessionListener());
+//        return registrationBean;
+//    }
 
 }
